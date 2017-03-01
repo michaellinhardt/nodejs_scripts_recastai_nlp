f01282c8d4235368b17d021ede498ca4d8b8cdf2
Michael Linhardt <michael.linhardt@recast.ai>
2017-03-01 15:13:11 +0100

    [ARC] clean empty starter

---
+++ src/helper.js
@@ -0,0 +1,55 @@
+import cfg from '../config/private'
+import _ from 'lodash'
+
+export default class Helper {
+  constructor () {
+    this.bloc('Helper is constructed')
+  }
+
+  bloc (title, data, whiteline) {
+    if (whiteline) { global.log('.') }
+    global.log('############################################################')
+    global.log(`### ${title}`)
+    global.log('############################################################')
+    if (!data) { return }
+    if (typeof data === 'string') {
+      global.dump(data)
+    } else if (typeof data === 'object') {
+      _.each(data, (str) => { global.dump(str) })
+    }
+    global.log('############################################################')
+  }
+
+  post (url, data, token, log) {
+    this.bloc('POSTING DATA', { post_url: url, post_data: data })
+
+    if (token && token !== 0) { token = { Authorization: token } }
+    if (!token) { token = {} }
+
+    global.request.post(url).set(token).send(data).end((err, res) => {
+      if (err) {
+        if (log) { err = { 'Error Message': err, 'Res Dump': res } }
+        this.bloc('!POST RETURN ERROR!', err)
+      } else {
+        if (log) { url = { Url: url, 'Res Dump': res } }
+        this.bloc('POST RETURN SUCCESS', url)
+      }
+    })
+  }
+
+  err (method, err) {
+    this.bloc(`class Script, method: ${method}\r\n${err}`)
+  }
+
+  bcMessage (data) {
+    this.bloc('BOT CONNECTOR: data', data)
+
+    const url = `${cfg.connector.url}/users/${cfg.connector.userSlug}/bots/${cfg.connector.botId}/conversations/${data.content.conversation}/messages`
+    const message = [{
+      type: 'text',
+      content: `${data.content.attachment.content}`,
+    }]
+
+    this.post(url, { message, senderId: data.senderId }, cfg.connector.userToken)
+  }
+}
--- src/script.js
+++ src/script.js
@@ -1,74 +1,19 @@
-import cfg from '../config/private'
-import _ from 'lodash'
+import Helper from './helper'
 
-export default class Script {
-  constructor () {
-    this.bloc('Script class is constructed')
-    this.itimer = 0
-  }
+export default class Script extends Helper {
 
   async start () {
     this.bloc('Starting the script!')
     try {
-      this.operation({ data: 'lol' })
+      this.bloc('Hello')
     } catch (err) {
       this.bloc('ERROR IN START METHOD', err)
     }
+    process.exit(0)
   }
 
-  bloc (title, data, whiteline) {
-    if (whiteline) { global.log('.') }
-    global.log('############################################################')
-    global.log(`### ${title}`)
-    global.log('############################################################')
-    if (!data) { return }
-    if (typeof data === 'string') {
-      global.dump(data)
-    } else if (typeof data === 'object') {
-      _.each(data, (str) => { global.dump(str) })
-    }
-    global.log('############################################################')
-  }
-
-  post (url, data, token, log) {
-    this.bloc('POSTING DATA', { post_url: url, post_data: data })
-
-    if (token && token !== 0) { token = { Authorization: token } }
-    if (!token) { token = {} }
-
-    global.request.post(url).set(token).send(data).end((err, res) => {
-      if (err) {
-        if (log) { err = { 'Error Message': err, 'Res Dump': res } }
-        this.bloc('!POST RETURN ERROR!', err)
-      } else {
-        if (log) { url = { Url: url, 'Res Dump': res } }
-        this.bloc('POST RETURN SUCCESS', url)
-      }
-    })
-  }
-
-  err (method, err) {
-    this.bloc(`class Script, method: ${method}\r\n${err}`)
-  }
-
-  async operation (data) {
-    try {
-      this.bloc('OPERATION DONE', { data })
-    } catch (err) {
-      this.bloc('OPERATION ERROR', { data, err })
-    }
-    return undefined
-  }
-
-  bcMessage (data) {
-    this.bloc('BOT CONNECTOR: data', data)
-
-    const url = `${cfg.connector.url}/users/${cfg.connector.userSlug}/bots/${cfg.connector.botId}/conversations/${data.content.conversation}/messages`
-    const message = [{
-      type: 'text',
-      content: `${data.content.attachment.content}`,
-    }]
-
-    this.post(url, { message, senderId: data.senderId }, cfg.connector.userToken)
+  constructor () {
+    super()
+    this.bloc('Script class is extended from Helper')
   }
 }
