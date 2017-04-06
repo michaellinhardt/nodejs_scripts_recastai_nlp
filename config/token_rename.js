8e33bfd3cad7021e86bd6dc83dc82e3569d14925
Michael Linhardt <michael.linhardt@recast.ai>
2017-04-06 17:45:11 +0200

    [ADD] empty token config file description

---
+++ config/token_rename.js
@@ -0,0 +1,21 @@
+// rename this file in token.js and set your config
+module.exports = {
+  fork_intent:
+  {
+    user: 'michael-linhardt',
+    bot: 'fork-intent',
+    token: 'xxx',
+  },
+  lucas_sfr:
+  {
+    user: 'lucasdchamps',
+    bot: 'sfr-bot',
+    token: 'xxx',
+  },
+  sfr:
+  {
+    user: 'recast-ai',
+    bot: 'sfr',
+    token: 'xxx',
+  },
+}
