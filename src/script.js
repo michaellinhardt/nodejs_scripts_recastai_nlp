76e9c118a6042f256f7ce06ed79d62286529dc14
Michael Linhardt <michael.linhardt@recast.ai>
2017-04-06 15:38:57 +0200

    [ADD] awsome working script

--- src/script.js
+++ src/script.js
@@ -6,15 +6,21 @@ const source = {
   user: 'lucasdchamps',
   bot: 'sfr-bot',
   token: '67f986b5299181a7dd49de6ccce3429a',
-  intent: 'changer_date_prelevement',
+  intent: 'abonnement',
 }
 source.url = `https://api.recast.ai/v2/users/${source.user}/bots/${source.bot}`
 
+// const target = {
+//   user: 'michael-linhardt',
+//   bot: 'fork-intent',
+//   token: '1591381a501fc1de88051797076b81ea',
+//   intent: 'changer-forfait',
+// }
 const target = {
-  user: 'michael-linhardt',
-  bot: 'fork-intent',
-  token: '1591381a501fc1de88051797076b81ea',
-  intent: 'changer-forfait',
+  user: 'recast-ai',
+  bot: 'sfr',
+  token: '5b3f5d6f7a5bc2138558c5c24f60396e',
+  intent: 'souscrire-abonnement',
 }
 target.url = `https://api.recast.ai/v2/users/${target.user}/bots/${target.bot}`
 
